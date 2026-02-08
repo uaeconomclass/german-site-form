d1dc723f9f3247f35e5ab7289a14474531542541
Valentyn Moroz <uaeconomclass@gmail.com>
2026-02-08 10:21:31 +0200

    Use Inter font in plugin/form styles

--- wp-plugin/energieausweis-form/energieausweis-form.php
+++ wp-plugin/energieausweis-form/energieausweis-form.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Energieausweis Form
  * Description: Data-driven Energieausweis form embedded via shortcode, with draft storage on wg/nwg/misch posts.
- * Version: 0.1.7
+ * Version: 0.1.8
  * Author: uaeconomclass
  */
 
@@ -13,7 +13,7 @@ if (!defined('ABSPATH')) {
 define('EA_FORM_PLUGIN_FILE', __FILE__);
 define('EA_FORM_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('EA_FORM_PLUGIN_URL', plugin_dir_url(__FILE__));
-define('EA_FORM_PLUGIN_VERSION', '0.1.7');
+define('EA_FORM_PLUGIN_VERSION', '0.1.8');
 
 require_once EA_FORM_PLUGIN_DIR . 'includes/cpt.php';
 require_once EA_FORM_PLUGIN_DIR . 'includes/enqueue.php';
